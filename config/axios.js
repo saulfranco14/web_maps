3f5ed0873d649287378e65a335d7b1ca1bd470d2
saulfranco14 <saul.franco1420@gmail.com>
2023-08-17 16:38:50 -0600

    Se actualiza el endpoint

--- config/axios.js
+++ config/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let clientAxios = ({});
 clientAxios = axios.create({
-    baseURL: 'https://bff2-2806-2f0-90e1-5828-40e7-5670-3516-6233.ngrok-free.app',
+    baseURL: 'https://01c3-2806-2f0-90e1-5828-40e7-5670-3516-6233.ngrok-free.app',
     headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
